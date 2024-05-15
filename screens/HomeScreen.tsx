a6d270b39238200eb4c53436f81990c3dea8b5f5
Romain Spielmann <romain.spielmann@ornikar.com>
2024-05-15 22:12:14 +0200

    Initial commit

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export function HomeScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit HomeScreen component</Text>
+    </View>
+  );
+}
